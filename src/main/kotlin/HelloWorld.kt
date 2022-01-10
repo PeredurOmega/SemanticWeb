b87768b2c47491c914bd9a0727f41d83134acd7d
berengerpro <berengermd.pro@gmail.com>
2022-01-10 18:20:17 +0100

    Changes by pauls

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,6 +1,8 @@
-
-import mui.material.AppBar
+import csstype.Color
+import mui.material.*
 import react.*
+import react.dom.aria.ariaLabel
+import react.dom.html.ButtonType
 import react.dom.html.ReactHTML.button
 import react.dom.html.ReactHTML.div
 import react.dom.html.ReactHTML.form
