5f2b232eeb63c26cd1f55ece00a47797f61b748b
Mathis <mathisgoichon@gmail.com>
2022-01-14 17:33:48 +0100

    ihm: Map Result

--- src/main/kotlin/search/SearchPage.kt
+++ src/main/kotlin/search/SearchPage.kt
@@ -4,7 +4,11 @@ import navBar
 import react.FC
 import react.Props
 import react.dom.html.ReactHTML.div
+import react.dom.html.ReactHTML.p
+import tools.map.marker
+import tools.map.popup
 import tools.requireSCSS
+import kotlin.js.json
 
 val searchPage = FC<Props> {
     requireSCSS("search-page")
@@ -31,6 +35,25 @@ val searchPage = FC<Props> {
         }
         div {
             className = "map-results"
+            mapResult {
+                id = "map"
+                center = json("lat" to "46.71", "lng" to "1.72")
+                zoom = 6
+                tileSize = 1
+                attribution = "Grandes Ecoles"
+                url = "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+
+                repeat(5) {
+                    marker {
+                        position = json("lat" to "${kotlin.random.Random.Default.nextDouble(43.0, 50.0)}", "lng" to "${kotlin.random.Random.Default.nextDouble(-2.4,7.6)}")
+                        popup {
+                            p {
+                                +"popup text"
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
