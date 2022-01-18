9d74d688f4cf717f57980da71f1ce60ad1a3b7f9
pauls <paul.souteyrat@timeplanner-app.com>
2022-01-18 09:34:35 +0100

    Adding syntaxic sugar to sparql query loader

--- src/main/kotlin/tools/sparql/QueriesProcessor.kt
+++ src/main/kotlin/tools/sparql/QueriesProcessor.kt
@@ -3,7 +3,7 @@ package tools.sparql
 import kotlinext.js.jso
 import react.*
 
-fun <V: SparqlVariables, R: SparqlResponse> useSparqlQuery(sparqlQuery : SparqlQuery<V, R>, variables: V): R? {
+fun <V : SparqlVariables, R : SparqlResponse> useSparqlQuery(sparqlQuery: SparqlQuery<V, R>, variables: V): R? {
     val (response, setResponse) = useState<R?>(null)
 
     useEffectOnce {
@@ -13,19 +13,33 @@ fun <V: SparqlVariables, R: SparqlResponse> useSparqlQuery(sparqlQuery : SparqlQ
     return response
 }
 
-external interface SparqlQueryLoaderProps : PropsWithChildren {
-    var sparqlQuery : SparqlQuery<*,*>
-    var variables : SparqlVariables
-}
-
 external interface SparqlQueryConsumerProps : Props {
-    var queryResult : SparqlResponse
+    var queryResult: SparqlResponse
 }
 
-val sparqlQueryLoader = FC<SparqlQueryLoaderProps> { props ->
-    val queryResult = useSparqlQuery(props.sparqlQuery, props.variables.asDynamic())
-    if (queryResult != null ) {
-        val element = cloneElement(Children.only(props.children), jso<SparqlQueryConsumerProps>{ this.queryResult = queryResult })
+fun <V : SparqlVariables, R : SparqlResponse> ChildrenBuilder.sparqlQueryLoader(
+    sparqlQuery: SparqlQuery<V, R>,
+    variables: V,
+    block: ChildrenBuilder.() -> Unit
+) {
+    sparqlQueryLoader {
+        this.sparqlQuery = sparqlQuery.unsafeCast<SparqlQuery<SparqlVariables, SparqlResponse>>()
+        this.variables = variables
+        block()
+    }
+}
+
+private external interface SparqlQueryLoaderProps<V : SparqlVariables, R : SparqlResponse> : PropsWithChildren {
+    var sparqlQuery: SparqlQuery<V, R>
+    var variables: V
+}
+
+private val sparqlQueryLoader = FC<SparqlQueryLoaderProps<SparqlVariables, SparqlResponse>> { props ->
+    val queryResult = useSparqlQuery(props.sparqlQuery, props.variables)
+    if (queryResult != null) {
+        val element = cloneElement(
+            Children.only(props.children),
+            jso<SparqlQueryConsumerProps> { this.queryResult = queryResult })
         child(element)
     }
 }
