8b9a31f840217aa0dc5d553c32c15a86c7ab8b54
tverstraet <timothe.verstraete@insa-lyon.fr>
2022-01-21 17:43:58 +0100

    nav bar beginning css

--- src/main/kotlin/NavBar.kt
+++ src/main/kotlin/NavBar.kt
@@ -2,13 +2,17 @@ import react.FC
 import react.Props
 import react.dom.html.ReactHTML.input
 import react.dom.html.ReactHTML.nav
+import react.router.dom.Link
 import tools.basicSVG
 import tools.requireSCSS
 
 val navBar = FC<Props> {
     requireSCSS("navbar")
     nav {
-        basicSVG("GesearchLogoSmall", "Logo de Gesearch")
+        Link {
+            to = "/"
+            basicSVG("GesearchLogoSmall", "Logo de Gesearch")
+        }
         input {
             className = "search-input"
         }
